7c37f9e048aa90051a3967802798489c99575a2d
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-01-29 22:35:20 +0530

    Dynamic projection

--- SpringDataJPA-DynamicProjection/src/main/java/com/myLearning/springApp/bo/VaccineDetails.java
+++ SpringDataJPA-DynamicProjection/src/main/java/com/myLearning/springApp/bo/VaccineDetails.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class VaccineDetails {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
